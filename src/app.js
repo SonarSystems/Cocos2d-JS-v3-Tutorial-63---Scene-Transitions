d7b0595d75e173b6364422147ff7e03e96f50da8
Frahaan Hussain <Sonar Systems>
2014-10-14 08:56:21 +0100

    Scene Transitions

---
+++ src/app.js
@@ -0,0 +1,56 @@
+var INITIALIZED = false;
+
+var HelloWorldLayer = cc.Layer.extend({
+    sprite:null,
+    ctor:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask the window size
+        var size = cc.winSize;
+
+        var sprite = new cc.Sprite.create(res.CloseNormal_png);
+        sprite.setAnchorPoint(cc.p(0.5, 0.5));
+        sprite.setPosition(cc.p(size.width / 2, size.height / 2));
+        //this.addChild(sprite, 0);
+
+        var menuItem1 = new cc.MenuItemFont("Push", play);
+        var menu = new cc.Menu(menuItem1);
+        menu.alignItemsVertically();
+        this.addChild(menu);
+
+        return true;
+    }
+});
+
+var play = function()
+{
+    var scene = new HelloWorldScene2();
+    // Fade Transition
+    //cc.director.runScene(new cc.TransitionFade(3.0, scene));
+
+    // Jump and Zoom Transition
+    //cc.director.runScene(new cc.TransitionJumpZoom(3.0, scene));
+
+    // Zoom and Flip Transition
+    cc.director.runScene(new cc.TransitionZoomFlipY(3.0, scene));
+};
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+
+        if (INITIALIZED == false)
+        {
+        	INITIALIZED = true;
+
+        	var layer = new HelloWorldLayer();
+        	this.addChild(layer);
+        }
+    }
+});
+
---
+++ src/app2.js
@@ -0,0 +1,49 @@
+var INITIALIZED_2 = false;
+
+var HelloWorldLayer2 = cc.Layer.extend({
+    sprite:null,
+    ctor:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask the window size
+        var size = cc.winSize;
+
+        var sprite = new cc.Sprite.create(res.CloseNormal_png);
+        sprite.setAnchorPoint(cc.p(0.5, 0.5));
+        sprite.setPosition(cc.p(size.width / 2, size.height / 2));
+        //this.addChild(sprite, 0);
+
+        var menuItem1 = new cc.MenuItemFont("Pop", pop);
+        var menu = new cc.Menu(menuItem1);
+        menu.alignItemsVertically();
+        this.addChild(menu);
+
+        return true;
+    }
+});
+
+var pop = function()
+{
+    INITIALIZED_2 = false;
+    cc.director.popScene();
+};
+
+var HelloWorldScene2 = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+
+        if (INITIALIZED_2 == false)
+        {
+            INITIALIZED_2 = true;
+
+            var layer = new HelloWorldLayer2();
+            this.addChild(layer);
+        }
+    }
+});
+
